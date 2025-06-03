b7c6d9b39b589e4317658cc7cd457a39506f6b62
Rohit Rana <92220893+Luanier@users.noreply.github.com>
2025-06-04 01:30:48 +0530

    Update img_util.py

--- basicsr/utils/img_util.py
+++ basicsr/utils/img_util.py
@@ -1,9 +1,3 @@
-# ------------------------------------------------------------------------
-# Copyright (c) 2022 megvii-model. All Rights Reserved.
-# ------------------------------------------------------------------------
-# Modified from BasicSR (https://github.com/xinntao/BasicSR)
-# Copyright 2018-2020 BasicSR Authors
-# ------------------------------------------------------------------------
 import cv2
 import math
 import numpy as np
@@ -14,15 +8,6 @@ from torchvision.utils import make_grid
 
 def img2tensor(imgs, bgr2rgb=True, float32=True):
     """Numpy array to tensor.
-
-    Args:
-        imgs (list[ndarray] | ndarray): Input images.
-        bgr2rgb (bool): Whether to change bgr to rgb.
-        float32 (bool): Whether to change to float32.
-
-    Returns:
-        list[tensor] | tensor: Tensor images. If returned results only have
-            one element, just return tensor.
     """
 
     def _totensor(img, bgr2rgb, float32):
@@ -44,21 +29,6 @@ def tensor2img(tensor, rgb2bgr=True, out_type=np.uint8, min_max=(0, 1)):
 
     After clamping to [min, max], values will be normalized to [0, 1].
 
-    Args:
-        tensor (Tensor or list[Tensor]): Accept shapes:
-            1) 4D mini-batch Tensor of shape (B x 3/1 x H x W);
-            2) 3D Tensor of shape (3/1 x H x W);
-            3) 2D Tensor of shape (H x W).
-            Tensor channel should be in RGB order.
-        rgb2bgr (bool): Whether to change rgb to bgr.
-        out_type (numpy type): output types. If ``np.uint8``, transform outputs
-            to uint8 type with range [0, 255]; otherwise, float type with
-            range [0, 1]. Default: ``np.uint8``.
-        min_max (tuple[int]): min and max values for clamp.
-
-    Returns:
-        (Tensor or list): 3D ndarray of shape (H x W x C) OR 2D ndarray of
-        shape (H x W). The channel order is BGR.
     """
     if not (torch.is_tensor(tensor) or
             (isinstance(tensor, list)
@@ -107,15 +77,6 @@ def tensor2img(tensor, rgb2bgr=True, out_type=np.uint8, min_max=(0, 1)):
 def imfrombytes(content, flag='color', float32=False):
     """Read an image from bytes.
 
-    Args:
-        content (bytes): Image bytes got from files or other streams.
-        flag (str): Flags specifying the color type of a loaded image,
-            candidates are `color`, `grayscale` and `unchanged`.
-        float32 (bool): Whether to change to float32., If True, will also norm
-            to [0, 1]. Default: False.
-
-    Returns:
-        ndarray: Loaded image array.
     """
     img_np = np.frombuffer(content, np.uint8)
     imread_flags = {
@@ -147,15 +108,6 @@ def padding(img_lq, img_gt, gt_size):
 def imwrite(img, file_path, params=None, auto_mkdir=True):
     """Write image to file.
 
-    Args:
-        img (ndarray): Image array to be written.
-        file_path (str): Image file path.
-        params (None or list): Same as opencv's :func:`imwrite` interface.
-        auto_mkdir (bool): If the parent folder of `file_path` does not exist,
-            whether to create it automatically.
-
-    Returns:
-        bool: Successful or not.
     """
     if auto_mkdir:
         dir_name = os.path.abspath(os.path.dirname(file_path))
@@ -166,12 +118,6 @@ def imwrite(img, file_path, params=None, auto_mkdir=True):
 def crop_border(imgs, crop_border):
     """Crop borders of images.
 
-    Args:
-        imgs (list[ndarray] | ndarray): Images with shape (h, w, c).
-        crop_border (int): Crop border for each end of height and weight.
-
-    Returns:
-        list[ndarray]: Cropped images.
     """
     if crop_border == 0:
         return imgs

f5ad5288f00ffe987be5aa6f0187d7fa1fe6e4a1
Rohit Rana <92220893+Luanier@users.noreply.github.com>
2025-06-04 01:14:03 +0530

    Update demo.py

--- basicsr/demo.py
+++ basicsr/demo.py
@@ -1,19 +1,12 @@
-# ------------------------------------------------------------------------
-# Copyright (c) 2022 megvii-model. All Rights Reserved.
-# ------------------------------------------------------------------------
-# Modified from BasicSR (https://github.com/xinntao/BasicSR)
-# Copyright 2018-2020 BasicSR Authors
-# ------------------------------------------------------------------------
+
 import torch
 
-# from basicsr.data import create_dataloader, create_dataset
+
 from basicsr.models import create_model
 from basicsr.train import parse_options
 from basicsr.utils import FileClient, imfrombytes, img2tensor, padding, tensor2img, imwrite
 
-# from basicsr.utils import (get_env_info, get_root_logger, get_time_str,
-#                            make_exp_dirs)
-# from basicsr.utils.options import dict2str
+
 
 def main():
     # parse options, set distributed setting, set ramdom seed

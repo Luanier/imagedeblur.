913dd5e6342e4192dcc87d38d34dd29092a5c91d
Rohit Rana <92220893+Luanier@users.noreply.github.com>
2025-06-04 01:30:15 +0530

    Update flow_util.py

--- basicsr/utils/flow_util.py
+++ basicsr/utils/flow_util.py
@@ -1,10 +1,4 @@
-# ------------------------------------------------------------------------
-# Copyright (c) 2022 megvii-model. All Rights Reserved.
-# ------------------------------------------------------------------------
-# Modified from BasicSR (https://github.com/xinntao/BasicSR)
-# Copyright 2018-2020 BasicSR Authors
-# ------------------------------------------------------------------------
-# Modified from https://github.com/open-mmlab/mmcv/blob/master/mmcv/video/optflow.py  # noqa: E501
+
 import cv2
 import numpy as np
 import os
@@ -13,15 +7,6 @@ import os
 def flowread(flow_path, quantize=False, concat_axis=0, *args, **kwargs):
     """Read an optical flow map.
 
-    Args:
-        flow_path (ndarray or str): Flow path.
-        quantize (bool): whether to read quantized pair, if set to True,
-            remaining args will be passed to :func:`dequantize_flow`.
-        concat_axis (int): The axis that dx and dy are concatenated,
-            can be either 0 or 1. Ignored if quantize is False.
-
-    Returns:
-        ndarray: Optical flow represented as a (h, w, 2) numpy array
     """
     if quantize:
         assert concat_axis in [0, 1]
@@ -56,15 +41,6 @@ def flowwrite(flow, filename, quantize=False, concat_axis=0, *args, **kwargs):
     If the flow is not quantized, it will be saved as a .flo file losslessly,
     otherwise a jpeg image which is lossy but of much smaller size. (dx and dy
     will be concatenated horizontally into a single image if quantize is True.)
-
-    Args:
-        flow (ndarray): (h, w, 2) array of optical flow.
-        filename (str): Output filepath.
-        quantize (bool): Whether to quantize the flow and save it to 2 jpeg
-            images. If set to True, remaining args will be passed to
-            :func:`quantize_flow`.
-        concat_axis (int): The axis that dx and dy are concatenated,
-            can be either 0 or 1. Ignored if quantize is False.
     """
     if not quantize:
         with open(filename, 'wb') as f:
@@ -87,14 +63,6 @@ def quantize_flow(flow, max_val=0.02, norm=True):
     After this step, the size of flow will be much smaller, and can be
     dumped as jpeg images.
 
-    Args:
-        flow (ndarray): (h, w, 2) array of optical flow.
-        max_val (float): Maximum value of flow, values beyond
-                        [-max_val, max_val] will be truncated.
-        norm (bool): Whether to divide flow values by image width/height.
-
-    Returns:
-        tuple[ndarray]: Quantized dx and dy.
     """
     h, w, _ = flow.shape
     dx = flow[..., 0]
@@ -112,14 +80,7 @@ def quantize_flow(flow, max_val=0.02, norm=True):
 def dequantize_flow(dx, dy, max_val=0.02, denorm=True):
     """Recover from quantized flow.
 
-    Args:
-        dx (ndarray): Quantized dx.
-        dy (ndarray): Quantized dy.
-        max_val (float): Maximum value used when quantizing.
-        denorm (bool): Whether to multiply flow values with width/height.
 
-    Returns:
-        ndarray: Dequantized flow.
     """
     assert dx.shape == dy.shape
     assert dx.ndim == 2 or (dx.ndim == 3 and dx.shape[-1] == 1)
@@ -163,15 +124,7 @@ def quantize(arr, min_val, max_val, levels, dtype=np.int64):
 def dequantize(arr, min_val, max_val, levels, dtype=np.float64):
     """Dequantize an array.
 
-    Args:
-        arr (ndarray): Input array.
-        min_val (scalar): Minimum value to be clipped.
-        max_val (scalar): Maximum value to be clipped.
-        levels (int): Quantization levels.
-        dtype (np.type): The type of the dequantized array.
 
-    Returns:
-        tuple: Dequantized array.
     """
     if not (isinstance(levels, int) and levels > 1):
         raise ValueError(

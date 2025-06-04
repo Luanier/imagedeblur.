5fc018338497a0ba04f421a646d535779c495a63
Rohit Rana <92220893+Luanier@users.noreply.github.com>
2025-06-04 09:01:57 +0530

    Update losses.py

--- basicsr/models/losses/losses.py
+++ basicsr/models/losses/losses.py
@@ -1,9 +1,4 @@
-# ------------------------------------------------------------------------
-# Copyright (c) 2022 megvii-model. All Rights Reserved.
-# ------------------------------------------------------------------------
-# Modified from BasicSR (https://github.com/xinntao/BasicSR)
-# Copyright 2018-2020 BasicSR Authors
-# ------------------------------------------------------------------------
+
 import torch
 from torch import nn as nn
 from torch.nn import functional as F
@@ -24,18 +19,10 @@ def mse_loss(pred, target):
     return F.mse_loss(pred, target, reduction='none')
 
 
-# @weighted_loss
-# def charbonnier_loss(pred, target, eps=1e-12):
-#     return torch.sqrt((pred - target)**2 + eps)
-
 
 class L1Loss(nn.Module):
     """L1 (mean absolute error, MAE) loss.
 
-    Args:
-        loss_weight (float): Loss weight for L1 loss. Default: 1.0.
-        reduction (str): Specifies the reduction to apply to the output.
-            Supported choices are 'none' | 'mean' | 'sum'. Default: 'mean'.
     """
 
     def __init__(self, loss_weight=1.0, reduction='mean'):
@@ -48,23 +35,12 @@ class L1Loss(nn.Module):
         self.reduction = reduction
 
     def forward(self, pred, target, weight=None, **kwargs):
-        """
-        Args:
-            pred (Tensor): of shape (N, C, H, W). Predicted tensor.
-            target (Tensor): of shape (N, C, H, W). Ground truth tensor.
-            weight (Tensor, optional): of shape (N, C, H, W). Element-wise
-                weights. Default: None.
-        """
+
         return self.loss_weight * l1_loss(
             pred, target, weight, reduction=self.reduction)
 
 class MSELoss(nn.Module):
     """MSE (L2) loss.
-
-    Args:
-        loss_weight (float): Loss weight for MSE loss. Default: 1.0.
-        reduction (str): Specifies the reduction to apply to the output.
-            Supported choices are 'none' | 'mean' | 'sum'. Default: 'mean'.
     """
 
     def __init__(self, loss_weight=1.0, reduction='mean'):
@@ -77,13 +53,7 @@ class MSELoss(nn.Module):
         self.reduction = reduction
 
     def forward(self, pred, target, weight=None, **kwargs):
-        """
-        Args:
-            pred (Tensor): of shape (N, C, H, W). Predicted tensor.
-            target (Tensor): of shape (N, C, H, W). Ground truth tensor.
-            weight (Tensor, optional): of shape (N, C, H, W). Element-wise
-                weights. Default: None.
-        """
+      
         return self.loss_weight * mse_loss(
             pred, target, weight, reduction=self.reduction)
 
